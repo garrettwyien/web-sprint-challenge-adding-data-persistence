f624ae80de94bbceff161ba7e8bfde94f92b4983
Garrett Yien <yien.garrett94@gmail.com>
2021-09-10 15:03:14 -0400

    built task middleware

---
+++ api/task/middleware.js
@@ -0,0 +1,35 @@
+const db = require('../../data/dbConfig');
+
+const validateTask = async (req,res,next) =>{
+    try {
+        if (!req.body.task_description ||
+            typeof req.body.task_description !== 'string' ||
+            req.body.task_description.length < 1) {
+                next({ status: 400, message: "invalid task"})
+            } else {
+                next()
+            }
+    } catch (err) {
+        next(err)
+    }
+}
+
+const checkProjectId = async (req,res,next)=>{
+    try {
+        const possibleProject = await db('projects')
+            .where('project_id', req.params.project_id)
+            .first()
+        if (!possibleProject) {
+            next({status:404,message:`project with project_id ${req.params.project_id} not found`})
+        } else {
+            next()
+        }
+    } catch (err) {
+        next(err)
+    }
+}
+
+module.exports = {
+    validateTask,
+    checkProjectId
+  }
--- api/task/router.js
+++ api/task/router.js
@@ -1,6 +1,10 @@
 // build your `/api/tasks` router here
 const express = require('express');
 const Task = require('./model');
+const {
+    validateTask,
+    checkProjectId
+  } = require('./middleware');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
@@ -11,7 +15,7 @@ router.get('/', (req, res, next) => {
     .catch(next)
 })
 
-router.post('/', (req,res,next) => {
+router.post('/', checkProjectId, validateTask, (req,res,next) => {
     const newTask = req.body
     Task.add(newTask)
     .then(task=>{
